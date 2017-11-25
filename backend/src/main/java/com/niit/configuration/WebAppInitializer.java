06711fea2374d31f5bf92412e9b495a9f4a38128
Jayanth7778 <gundalajayanthkumargoud@gmail.com>
2017-11-25 16:19:01 +0530

    backend

---
+++ backend/src/main/java/com/niit/configuration/WebAppInitializer.java
@@ -0,0 +1,28 @@
+package com.niit.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer
+{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings()
+	{
+		// TODO Auto-generated method stub
+		return new String[]{"/"}; //any requests, forward the request to DispatcherServlet
+	}
+}
