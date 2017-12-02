60ca77169c2ec013e30295f31f5b566688da2b38
Jayanth7778 <gundalajayanthkumargoud@gmail.com>
2017-12-02 20:37:53 +0530

    middleware

--- middleware/src/main/java/com/niit/controller/PersonController.java
+++ middleware/src/main/java/com/niit/controller/PersonController.java
@@ -3,23 +3,27 @@ package com.niit.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.niit.dao.PersonDAO;
+import com.niit.model.ErrorClazz;
 import com.niit.model.Person;
 
 @Controller
 public class PersonController
 {
 	@Autowired
-	private PersonDAO personDAO;
+	private PersonDAO personDao;
 	public PersonController()
 	{
-		System.out.println("CREATING INSTANCE FOR PERSONCONTROLLER");
+		System.out.println("CREATING INSTANCE FOR PERSON CONTROLLER");
 	}
 	
 	@RequestMapping(value="/getallpersons",method=RequestMethod.GET)
@@ -27,7 +31,7 @@ public class PersonController
 	//@ResponseBody - convert list of Java objects to array of JSON 
 	public @ResponseBody List<Person> getAllPersons()
 	{
-		List<Person> persons=personDAO.getAllPersons();
+		List<Person> persons=personDao.getAllPersons();
 		return persons;
 	}
 	
@@ -37,7 +41,30 @@ public class PersonController
 	@RequestMapping(value="/saveperson",method=RequestMethod.POST)
 	public @ResponseBody Person savePerson(@RequestBody Person person)
 	{
-		personDAO.savePerson(person);
+		personDao.savePerson(person);
 		return person;
+	}
+	
+	@RequestMapping(value="/deleteperson/{id}",method=RequestMethod.DELETE)
+	public ResponseEntity<?> deletePerson(@PathVariable int id)
+	{		
+		try
+		{
+			personDao.deletePerson(id);
+		}
+		catch(Exception e)
+		{
+			ErrorClazz error=new ErrorClazz(1,"Person with " + id+ " doesn't exists");
+			return new ResponseEntity<ErrorClazz>(error,HttpStatus.NOT_FOUND);
+			// 2nd call back function [Error function]
+			//response.data = ErrorClazz obj in JSON
+			//response.status=HttpStatus.NOT_FOUND 404
+		}		
+		
+		List<Person> persons=personDao.getAllPersons();
+		return new ResponseEntity<List<Person>>(persons,HttpStatus.OK);
+		//1st call back function [Success function
+		//response.data= List<Person> in JSON
+		//response.status= HttpStatus.OK, 200
 	}
 }
